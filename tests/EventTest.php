5162d5bd93699f43d020ebdb54edbf6d313d2c62
Chris Schuld <cbschuld@gmail.com>
2022-06-19 13:52:44 -0700

    added tests, testing workflow

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -16,6 +16,16 @@ class EventTest extends TestCase
         $this->assertEquals($results->getStatusCode(), 200);
     }
 
+    public function testInvalidKey()
+    {
+        $this->assertNotEmpty(getenv('ANTICIPATED_IO_KEY'));
+        $key = 'f14847a5-b6a2-45c2-a152-30d0295cd9ee';
+        $a = new \AnticipatedIO\Events(['key' => $key]);
+        $results = $a->createJson('+5 min', 'https://diagnostics.anticipated.io/v1/post', 'post', ['test' => 'php']);
+        $this->assertFalse($results->getSuccess());
+        $this->assertEquals($results->getStatusCode(), 403);
+    }
+
     public function testCreateInPast()
     {
         $this->assertNotEmpty(getenv('ANTICIPATED_IO_KEY'));
@@ -35,7 +45,6 @@ class EventTest extends TestCase
         $dte->modify('+5 min');
         $dateTimeFormatted = $dte->format(\DateTime::ATOM);
         $results = $a->createJson($dateTimeFormatted, 'https://diagnostics.anticipated.io/v1/post', 'post', ['test' => 'php']);
-        var_dump($results);
         $this->assertTrue($results->getSuccess());
         $this->assertEquals($results->getStatusCode(), 200);
         $this->assertEquals($results->getEvent()->getWhen()->format(\DateTimeInterface::ISO8601), $dte->format(\DateTimeInterface::ISO8601));
